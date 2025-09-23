6967547e5c6533fdcb6a3df8eb9ad4093356c91e
majd774 <majdhisham9@gmail.com>
2025-09-23 23:14:26 +0300

    Add About page with team, achievements, and technologies sections
    - Created About page
    - Added sections for Our Achievements, The Team, and Technologies Used
    - Linked styling with about.css

---
+++ m4n/src/pages/about/about.jsx
@@ -0,0 +1,33 @@
+import './about.css'
+
+function About(){
+    return(
+        <div className='about-container'>
+             <h1 className='main-title'>about</h1>
+             <div className='section-box'>
+                <h2 className='section-title'>Our Achievements</h2>
+<p>We have created a modern movie and series build a plateform where users can browse,rate,and discover new films and shows. we focused on delivering a smooth and interactive user experience</p> 
+</div>
+<div className='section-box'>
+    <h2 className='section-title'>The Team</h2>
+    <ul className='team-list'>
+        <li>Majd Hisham</li>
+        <li>Markb Hajam</li>
+        <li>Mohammed Al-Warith</li>
+        <li>Mohammed Saboor</li>
+        <li>Naji Al-Lswas</li>
+    </ul>
+</div>
+<div className='section-box'>
+    <h2 className='section-title'>Technologies Used</h2>
+    <h1>REACT , Vite</h1>
+
+</div>
+
+
+
+
+
+        </div>
+     ) 
+} export default About
