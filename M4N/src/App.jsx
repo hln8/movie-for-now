b5b5eb402af8035bb2839d5a38499ea3ba00af00
Mohammed Sabor <232431633+hln8@users.noreply.github.com>
2025-09-24 14:37:26 +0300

    footer component
    اضافه فوتر لصفحه الويب وتختفي في صفحه الجوال

--- M4N/src/App.jsx
+++ M4N/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
+import Footer from './components/footer/footer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
