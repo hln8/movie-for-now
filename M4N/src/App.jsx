c9cd03181e1405386e3279c665a7f2e124cc1178
Mohammmed-Alw <2000tcog@gmail.com>
2025-10-15 19:55:38 +0300

    feat: Add Movie Details page and update App route

--- M4N/src/App.jsx
+++ M4N/src/App.jsx
@@ -4,18 +4,23 @@ import Home from './pages/home/Home';
 import Footer from './components/footer/footer';
 import NotFound from './components/404 page/notFound';
 import AwardsPage from './pages/Awards Page/AwardsPage';
+import { AuthProvider } from './context/AuthContext';
+import MovieDetails from './pages/MovieDetails/MovieDetails';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/awards' element={<AwardsPage />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path="/movie/:id" element={<MovieDetails />}  />
+          <Route path='/awards' element={<AwardsPage />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 export default App
